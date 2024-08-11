ec2e1fe8dd844e77f960e72c5cbb705aedb14cbd
B3ny4m1n74f <87936204+B3ny4m1n74f@users.noreply.github.com>
2024-08-11 19:56:04 -0400

    Add files via upload

---
+++ routes/pages.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/chat', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/chat.html'));
+});
+
+router.get('/gpt-chat', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/gpt-chat.html'));
+});
+
+router.get('/admin', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/admin.html'));
+});
+
+router.get('/faq-management', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/faq-management'));
+});
+
+router.get('/upload', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/upload.html'));
+});
+
+router.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/register.html'));
+});
+
+
+module.exports = router;
+
+
+
+
+function yetAnotherFunction() {
+    // |--202407-MacOS--|
+    // |--Another:Shuaijun Liu--|
+    console.log("Another:Shuaijun Liu");
+}
