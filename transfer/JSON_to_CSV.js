d40186632b1bacc90eeca9929edda619615acab2
B3ny4m1n74f <87936204+B3ny4m1n74f@users.noreply.github.com>
2024-08-17 20:56:22 -0400

    Add files via upload

--- transfer/JSON_to_CSV.js
+++ transfer/JSON_to_CSV.js
@@ -2,42 +2,42 @@ const fs = require('fs');
 const path = require('path');
 const { Parser } = require('json2csv');
 
-// Set the directory where the files are located (one level up)
-const dataDir = path.join(__dirname, '../data');
-
-// Read the JSON file from the data directory
+const dataDir = process.env.DATA_DIR || path.join(__dirname, '../data');
 const jsonFilePath = path.join(dataDir, 'BU_MET_FAQs.json');
 const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
 
-// Define the fields to be converted
 const fields = ['module', 'question', 'answer'];
 const opts = { fields };
 
-// Function to convert Markdown links to HTML links
 function convertMarkdownToHtml(text) {
     return text.replace(/\[([^\]]+)\]\(([^)]+)\)/g, '<a href="$2">$1</a>');
 }
 
-// Convert JSON data
+function convertMarkdownFields(entry, fields) {
+    fields.forEach(field => {
+        if (entry[field]) {
+            entry[field] = convertMarkdownToHtml(entry[field]);
+        }
+    });
+}
+
+// Apply conversion to relevant fields
 jsonData.forEach(entry => {
-    entry.answer = convertMarkdownToHtml(entry.answer);
+    convertMarkdownFields(entry, ['answer']); // Add more fields as needed
 });
 
-// Define the path for the CSV file in the data directory
 const csvFilePath = path.join(dataDir, 'BU_MET_FAQs.csv');
 
 // Check if the CSV file already exists and delete it if it does
 if (fs.existsSync(csvFilePath)) {
-    fs.unlinkSync(csvFilePath);  // Delete the existing file
+    fs.unlinkSync(csvFilePath);
     console.log('Existing BU_MET_FAQs.csv file deleted.');
 }
 
-// Use json2csv to convert JSON to CSV
 try {
     const parser = new Parser(opts);
     const csv = parser.parse(jsonData);
 
-    // Write the CSV to a file in the data directory
     fs.writeFileSync(csvFilePath, csv, 'utf8');
     console.log('CSV file successfully created:', csvFilePath);
 } catch (err) {
